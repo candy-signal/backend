cffed56cd1669f7d68f14a6de591a1dc657fec18
jun <ggprgrkjh@naver.com>
2023-08-26 19:12:29 +0900

    feat: CandyRepository

---
+++ src/main/java/candysignal/candy/repository/CandyRepository.java
@@ -0,0 +1,8 @@
+package candysignal.candy.repository;
+
+import candysignal.candy.entity.Candy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandyRepository extends JpaRepository<Candy,Long> {
+
+}
