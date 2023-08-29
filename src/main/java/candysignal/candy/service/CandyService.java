164c1f8fdb286eeb902b3f0c3eda1a2ac5b3fb6d
jun <ggprgrkjh@naver.com>
2023-08-29 23:40:23 +0900

    feat: create CandyService

---
+++ src/main/java/candysignal/candy/service/CandyService.java
@@ -0,0 +1,13 @@
+package candysignal.candy.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CandyService {
+
+}
